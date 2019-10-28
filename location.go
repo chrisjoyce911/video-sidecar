428d8952d80d84ee082907fad03caccb0c4a2e30
Chris Joyce <chrisj@benon.com>
2019-10-28 10:07:15 +1000

    Basic models first pass

---
+++ location.go
@@ -0,0 +1,23 @@
+package videosidecar
+
+// Filmed location
+type Location struct {
+	LocDisplayName string
+	LocLat         float64
+	LocLong        float64
+	LocCategory    string
+	LocType        string
+	LocName        string
+	LocHouseNr     string
+	LocStreet      string
+	LocSuburb      string
+	LocCity        string
+	LocPostcode    string
+	LocCounty      string
+	LocState       string
+	LocCountry     string
+	LocCountryCode string
+	LocDescription string
+	LocNotes       string
+	LocFavorite    bool
+}
